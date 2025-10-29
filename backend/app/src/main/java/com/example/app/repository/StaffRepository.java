775f21f7b032307049260ec1332317bff9f6487e
K2ghb003 <spr2440003@stu.o-hara.ac.jp>
2025-10-29 11:01:26 +0900

    Repository 追加

---
+++ backend/app/src/main/java/com/example/app/repository/StaffRepository.java
@@ -0,0 +1,11 @@
+package com.example.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.app.entity.Staff;
+
+@Repository
+public interface StaffRepository extends JpaRepository<Staff, String> {
+    // List<Staff> findByStaffName(String name);
+}
