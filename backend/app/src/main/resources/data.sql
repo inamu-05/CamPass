b435b8f391f38118c3eb44cc7eefbf4af8491079
K2ghb003 <spr2440003@stu.o-hara.ac.jp>
2025-11-06 09:51:50 +0900

    DBフリガナ修正

--- backend/app/src/main/resources/data.sql
+++ backend/app/src/main/resources/data.sql
@@ -1,4 +1,9 @@
 -- INSERTのみに使ってください。(既にデータが存在する場合は無視されます。)
 -- UPDATEやDELETEは使用しないでください。
 
+
+-- INSERT QUERIES:
+DELETE IGNORE FROM course;
+INSERT IGNORE INTO course (course_id, course_name) VALUES ('01', '情報');
+
 INSERT IGNORE INTO course (course_id, course_name) VALUES ('02', '簿記');
