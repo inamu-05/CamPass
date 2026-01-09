b8257acfdf0be63d96484257b1d7f02c551231c8
aira-tachi <spr2440851@stu.o-hara.ac.jp>
2026-01-09 14:05:25 +0900

    コード修正

--- backend/app/src/main/java/com/example/app/entity/User.java
+++ backend/app/src/main/java/com/example/app/entity/User.java
@@ -11,7 +11,6 @@ import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
